d4433e4b6b07cdafc53f1df65d5b33e5f8c8b471
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2023-12-27 18:00:19 +0000

    Update rh.py

--- rh.py
+++ rh.py
@@ -74,24 +74,30 @@ class RHScreen(): # inicializa a classe RH
 
             # Widgets - [Molduras]
 
-            self.canvas_bt = Canvas(self.frame2, bd = 4, bg = 'grey', highlightbackground = 'black', highlightthickness = 3) # objeto recebe uma moldura
-            self.canvas_bt.place(relx = 0.01, rely = 0.01, relwidth = 0.98, relheight = 0.65)                                # posiciona a moldura
+            self.canvas_bt = Canvas(self.frame2, bd=4, bg='grey', highlightbackground='grey', highlightthickness=3, relief='sunken') # objeto recebe uma moldura
+            self.canvas_bt.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.65)  # posiciona a moldura
 
-            self.canvas_bt = Canvas(self.frame2, bd = 4, bg = 'grey', highlightbackground = 'black', highlightthickness = 3) # objeto recebe uma moldura
-            self.canvas_bt.place(relx = 0.01, rely = 0.67, relwidth = 0.98, relheight = 0.32)                                # posiciona a moldura
+            self.canvas_bt = Canvas(self.frame2, bd=4, bg='grey', highlightbackground='grey', highlightthickness=3, relief='sunken') # objeto recebe uma moldura
+            self.canvas_bt.place(relx=0.01, rely=0.67, relwidth=0.98, relheight=0.32)  # posiciona a moldura        
 
             #--------------------------------------
 
             # Widgets - [Labels]
 
             self.lb_title1 = Label(self.frame2, text = 'Personal Info', bg='grey', font=('comic-sans', 15, 'bold', 'italic')) # setup
-            self.lb_title1.place(relx=0.33, rely=0.027, relwidth=0.3, relheight=0.05)                                         # posicao
+            self.lb_title1.place(relx=0.33, rely=0.03, relwidth=0.3, relheight=0.05)                                          # posicao
 
             self.lb_name = Label(self.frame2, text = 'Name:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
             self.lb_name.place(relx=0.14, rely=0.1, relwidth=0.09, relheight=0.05)                                  # posicao
 
-            self.lb_birth = Label(self.frame2, text = 'Age:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
-            self.lb_birth.place(relx=0.16, rely=0.16, relwidth=0.07, relheight=0.05)                                # posicao
+            self.lb_id = Label(self.frame2, text = 'ID:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
+            self.lb_id.place(relx=0.16, rely=0.16, relwidth=0.07, relheight=0.05)                               # posicao
+
+            self.lb_code = Label(self.frame2, text = 'Code:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
+            self.lb_code.place(relx=0.3, rely=0.16, relwidth=0.07, relheight=0.05)                                  # posicao
+
+            self.lb_age = Label(self.frame2, text = 'Age:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
+            self.lb_age.place(relx=0.46, rely=0.16, relwidth=0.07, relheight=0.05)                                  # posicao
 
             self.lb_sex = Label(self.frame2, text = 'Sex:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
             self.lb_sex.place(relx=0.16, rely=0.22, relwidth=0.06, relheight=0.05)                                # posicao
@@ -121,10 +127,10 @@ class RHScreen(): # inicializa a classe RH
             self.lb_pos.place(relx=0.065, rely=0.77, relwidth=0.16, relheight=0.05)                                          # posicao 
 
             self.lb_salary = Label(self.frame2, text = 'Salary:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
-            self.lb_salary.place(relx=0.12, rely=0.83, relwidth=0.1, relheight=0.05)                                    # posicao    
+            self.lb_salary.place(relx=0.13, rely=0.83, relwidth=0.1, relheight=0.05)                                    # posicao    
 
             self.lb_turn = Label(self.frame2, text = 'Work Shift:', bg='grey', font=('comic-sans', 10, 'bold', 'italic')) # setup
-            self.lb_turn.place(relx=0.08, rely=0.9, relwidth=0.14, relheight=0.05)                                       # posicao 
+            self.lb_turn.place(relx=0.085, rely=0.89, relwidth=0.14, relheight=0.05)                                        # posicao 
 
             #--------------------------------------
 
@@ -133,8 +139,14 @@ class RHScreen(): # inicializa a classe RH
             self.in_name = Entry(self.frame2, bd=4)                               # setup
             self.in_name.place(relx=0.23, rely=0.1, relwidth=0.7, relheight=0.05) # posicao
 
-            self.in_birth = Entry(self.frame2, bd=4)                                 # setup
-            self.in_birth.place(relx=0.23, rely=0.16, relwidth=0.05, relheight=0.05) # posicao
+            self.in_id = Entry(self.frame2, bd=4)                                 # setup
+            self.in_id.place(relx=0.23, rely=0.16, relwidth=0.05, relheight=0.05) # posicao
+
+            self.in_code = Entry(self.frame2, bd=4)                                 # setup
+            self.in_code.place(relx=0.39, rely=0.16, relwidth=0.05, relheight=0.05) # posicao
+
+            self.in_age = Entry(self.frame2, bd=4)                                 # setup
+            self.in_age.place(relx=0.54, rely=0.16, relwidth=0.05, relheight=0.05) # posicao
 
             self.in_sex = Entry(self.frame2, bd=4)                                 # setup
             self.in_sex.place(relx=0.23, rely=0.22, relwidth=0.04, relheight=0.05) # posicao
@@ -164,7 +176,7 @@ class RHScreen(): # inicializa a classe RH
             self.in_salary.place(relx=0.23, rely=0.83, relwidth=0.1, relheight=0.05) # posicao    
 
             self.in_turn = Entry(self.frame2, bd=4)                                # setup
-            self.in_turn.place(relx=0.23, rely=0.9, relwidth=0.04, relheight=0.05) # posicao 
+            self.in_turn.place(relx=0.23, rely=0.89, relwidth=0.04, relheight=0.05) # posicao 
 
             #--------------------------------------
 
